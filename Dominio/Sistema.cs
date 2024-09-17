a53452da922d9ef9aa13f3f45b337b52161bd968
Santiago <santiago.baillo@fi365.ort.edu.uy>
2024-09-16 22:14:51 -0300

    Agrego clase Sistema

---
+++ Dominio/Sistema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Sistema
+    {
+        private List<Agencia> _agencias = new List<Agencia>();
+        private List<Destino> _destinos = new List<Destino>();
+        private List<Paquete> _paquetes = new List<Paquete>();
+    }
+}
